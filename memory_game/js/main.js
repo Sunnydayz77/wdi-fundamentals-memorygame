24d41dd2f6f0ef4d565b13d386a59f8a9944a583
Jennifer Piwonski <jenpiwonski@gmail.com>
2020-01-17 15:10:57 -0500

    Memory Game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,59 @@
 console.log("Up and running!");
 
 let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
+let cardTwo = "queen";
+let cardThree = "king";
 let cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+//console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardFour);
+
+//const cards = ["queen", "queen", "king", "king"]
+
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/-king-of-diamonds"
+}
+];
+
+var cardsInPlay = [];
+
+var checkForMatch = function () {
+	if (cardsInPlay.length == 2) {
+
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+			} else {
+			alert("Sorry, try again.");
+			}
+	}
+};
+
+var flipCard = function (cardId) {	
+cardsInPlay.push(cards[cardId].rank);  
+console.log("User flipped" + " " + cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);  
+	checkForMatch();
+
+};		
+
+//Use console.log() to display the card that the user flipped//
+
